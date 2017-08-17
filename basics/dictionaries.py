048f42e937d5388b0ad1d9ff165c3694fd9848a9
pthiele <thieleone@gmail.com>
2017-08-17 02:40:16 +0200

    Dics expanded.

--- basics/dictionaries.py
+++ basics/dictionaries.py
@@ -6,3 +6,16 @@ def fib(n):
 	else:
 		return fib(n-1)+fib(n-2)
 print fib(4)
+
+def sort(list):
+	ref=list[0]
+	for j in range(1,len(list)):
+		for i in range (1,len(list)):
+			if list[i]<list[i-1]:
+				ref=list[i]
+				list[i]=list[i-1]
+				list[i-1]=ref
+	return list
+
+
+print(sort([5,4,3,2,1]))
