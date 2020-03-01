b90d107d8f4cef0b509e6c9a1cab012dc0cc30ac
Philipp <thieleone@gmail.com>
2020-03-01 12:25:48 +0100

    f-String added

--- pcs/schaef.py
+++ pcs/schaef.py
@@ -6,17 +6,17 @@ message3 = """Das ist ein
 Test ueber mehr
 ere Zeilen."""
 
-print(message[3:6])
-print(message[:7])
-print(message[10:])
-print(message.lower())
-print(message.count('p'))
-print(message.find('Test'))
+# print(message[3:6])
+# print(message[:7])
+# print(message[10:])
+# print(message.lower())
+# print(message.count('p'))
+# print(message.find('Test'))
 
 message = message.replace('Philipp','Mac')
-print(message)
-print(message2[7])
-print(len(message3))
+# print(message)
+# print(message2[7])
+# print(len(message3))
 
 
 greeting='Hello'
@@ -33,10 +33,10 @@ greeting_message4=greeting_message3.format(greeting,name)
 greeting_message5=f'{greeting}, {name.upper()}. Welcome!'
 
 
-print(greeting_message)
-print(greeting_message2)
-print(greeting_message4)
-print(greeting_message5)
+# print(greeting_message)
+# print(greeting_message2)
+# print(greeting_message4)
+# print(greeting_message5)
 
 
 #print(help(str))
@@ -44,8 +44,15 @@ print(greeting_message5)
 
 
 languages = ['Python', 'Java', 'C++', 'C#']
+languagesMessage=f'Eine Auflistung von Programmiersprachen:\n1. {languages[0]}\n2. {languages[1]}\n3. {languages[2]}\n4. {languages[3]}'
 
 
-print(languages[:2])
-print(languages[2:])
-print(languages[1:3])
+# print(languages[:2])
+# print(languages[2:])
+# print(languages[1:3])
+
+print (languagesMessage)
+
+
+
+
